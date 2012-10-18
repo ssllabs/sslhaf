0ca24bdefab20198390dcac702e4163ee346e81d
Ivan Ristic <ivanr@webkreator.com>
2012-10-18 10:56:50 +0100

    Change licence to BSD.

--- mod_sslhaf.c
+++ mod_sslhaf.c
@@ -1,15 +1,41 @@
 /*
- * mod_sslhaf: Apache module for passive SSL client fingerprinting
- * Copyright (c) 2009-2012 Qualys,Inc. All rights reserved.
- *
- * THIS PRODUCT IS NOT READY FOR PRODUCTION USE. DEPLOY AT YOUR OWN RISK.
- * 
- * This product is released under the terms of the General Public Licence
- * version 2 (GPLv2).
- *
- * NO WARRANTY. YOU MUST READ AND AGREE TO THE LICENCE BEFORE YOU USE THIS PROGRAM.
- */
+
+mod_sslhaf: Apache module for passive SSL client fingerprinting
  
+ | THIS PRODUCT IS NOT READY FOR PRODUCTION USE. DEPLOY AT YOUR OWN RISK.
+
+Copyright (c) 2009-2012, Qualys, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+* Neither the name of the Qualys, Inc. nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 /*
  * This Apache module will extract the list of SSL cipher suites
  * offered by HTTP clients during the SSL negotiation phase. The idea
